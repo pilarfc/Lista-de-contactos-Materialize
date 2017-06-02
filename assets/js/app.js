0e8b0facf0c65eb0b77f9879999128f4f7d9b16a
pilarfc <pilar.fc@outlook.com>
2017-06-02 12:58:44 +0000

    finalizando maquetacion

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,11 @@
- $(document).ready(function(){
+ 
+//Funcion para el navbar 
+
+$(".button-collapse").sideNav();
+
+
+// Función para el modal
+$(document).ready(function(){
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal').modal();
   });
