db7fd1224eb802ff094704c43643995109e33f60
pilarfc <pilar.fc@outlook.com>
2017-06-05 10:32:39 +0000

    generando plantilla

--- assets/js/app.js
+++ assets/js/app.js
@@ -26,13 +26,32 @@ var Obtenercontacto = function ($nombre, $telefono) {
 }
 
 
-var crearTarjetas = function () {
+/*var crearTarjetas = function () {
     
-    // Creando tarjetas de contacto     
+     Creando tarjetas de contacto     
     
     var tarjetaDeContacto = $('<div/>', {
         
         'id'    : 'tarjetaCompleta'
     });
     
-}
+    
+}*/
+
+
+var plantillaTarjeta = '<div class="col s12 m7" id="tarjetaCompleta">' +
+            '<div class="card horizontal" id="tarjetaCompleta">'+
+              '<div class="card-image">'+
+                '<img src="assets/img/flowers.jpg">'+
+              '</div>' +
+              '<div class="card-stacked">'+
+                '<div class="card-content" id="texto-contacto">' +
+                  '<p class="texto-tarjeta-nombre"> </p>'+
+                  '<p class="texto-tarjeta-telefono"><i class="material-icons">perm_identity</i> <p>__nombre__</p> </p>' +
+                  '<p class="texto-tarjeta-telefono"><i class="material-icons">phone</i> __telefono__ </p>'+
+                '</div>'+
+              '</div>'+
+            '</div>' +
+          '</div>'+
+    
+
